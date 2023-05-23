62df3b3b833530b5b78302eea4a4d35bccd28cbf
Alpana Lonare <alp.lonare@gmail.com>
2023-05-23 14:55:20 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EventDemo />
     </div>
   );
 }
+// MAP OPERATOR
+// <h1>Delhi</h1>
+function EventDemo() {
+  //let [list, setList] = useState(["delhi", "chennai"]);
+  let [list, setList] = useState(["todo1"]);
 
+  // Member Funcn
+  let addNewItem = () => {
+    // let newList = [...list, "kochi"];
+    //let newList = ["kochi", ...list];
+    // We should not use document object
+    let inputTag = document.querySelector("#inputid");
+    let inputValue = inputTag.value;
+
+    let newList = [inputValue, ...list];
+    setList(newList);
+  };
+
+  return (
+    <div>
+      <h1>List Demo </h1>
+
+      <input type="text" placeholder="Enter Todo" id="inputid" />
+      <input type="button" value="Add new Item" onClick={addNewItem} />
+
+      <hr />
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </div>
+  );
+}
 export default App;
