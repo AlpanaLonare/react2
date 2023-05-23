28d347dad077793660fc0135c691f2cc40a0780a
Alpana Lonare <alp.lonare@gmail.com>
2023-05-23 15:59:04 +0530

    todo-add-del

--- src/App.js
+++ src/App.js
@@ -6,24 +6,45 @@ function App() {
     </div>
   );
 }
-
+// MAP OPERATOR
+// <h1>Delhi</h1>
 function EventDemo() {
-  let [list, setList] = useState([" "]);
-  let addNewItem = () => {
-    let inputTag = document.querySelector("#inputid");
-    let inputValue = inputTag.value;
-    let newList = [inputValue, ...list];
-    setList(newList);
+  let [todo, setTodo] = useState("");
+  let [list, setList] = useState([]);
+  let inputHandler = (e) => {
+    console.log(e.target.value);
+    let newValue = e.target.value;
+    setTodo(newValue);
   };
+  let addNewItem = () => {
+    let newList = [todo, ...list];
+    setList(newList);
+    setTodo("");
+  };
+  let deleteTodo = (index) => {
+    list.splice(index, 1);
+    console.log(list);
+    setList([...list]);
+  };
+  // UI
   return (
-    <div >
-      <h1 className="bg-success">TODO</h1>
-      <input type="text" placeholder="Type here...." id="inputid" />
-      <input type="button" value="Add" onClick={addNewItem} />
+    <div>
+      <h1>List Demo </h1>
+      <h1 className="bg-success text-white p-3">Todo App </h1>
 
+      <input
+        type="text"
+        placeholder="Enter Todo"
+        value={todo}
+        onChange={inputHandler}
+      />
+      <input type="button" value="Add New Item" onClick={addNewItem} />
       <hr />
-      {list.map((item) => (
-        <h1>{item}</h1>
+      {list.map((item, index) => (
+        <div key={index} className="d-flex justify-content-between">
+          <h1>{item}</h1>
+          <input type="button" value="DEL" onClick={() => deleteTodo(index)} />
+        </div>
       ))}
     </div>
   );
