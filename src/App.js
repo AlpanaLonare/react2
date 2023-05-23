5225de6fc6eb80b586b0aa136bf1198d0b57b392
Alpana Lonare <alp.lonare@gmail.com>
2023-05-23 16:22:33 +0530

    success

--- src/App.js
+++ src/App.js
@@ -7,43 +7,36 @@ function App() {
   );
 }
 function EventDemo() {
-  let [todo, setTodo] = useState("");
-  let [list, setList] = useState([]);
-  let inputHandler = (e) => {
-    console.log(e.target.value);
-    let newValue = e.target.value;
-    setTodo(newValue);
+
+  let [counter, setCounter] = useState(100);
+  let [displayImage, setDisplayImage] = useState(
+    "https://picsum.photos/id/230/300/300"
+  );
+
+  let likeMe = () => {
+    counter = counter + 1;
+  
+    setCounter(counter);
   };
-  let addNewItem = () => {
-    let newList = [todo, ...list];
-    setList(newList);
-    setTodo("");
+  let changeImage = () => {
+    displayImage = "https://picsum.photos/id/238/300/300";
+    setDisplayImage(displayImage);
   };
-  let deleteTodo = (index) => {
-    list.splice(index, 1);
-    console.log(list);
-    setList([...list]);
+
+  let resetImage = () => {
+    displayImage = "https://picsum.photos/id/241/300/300";
+
+    setDisplayImage(displayImage);
   };
 
   return (
-    <div>
-      <h1>List Demo </h1>
-      <h1 className="bg-success text-white p-3">Todo App </h1>
-
-      <input
-        type="text"
-        placeholder="Enter Todo"
-        value={todo}
-        onChange={inputHandler}
-      />
-      <input type="button" value="Add New Item" onClick={addNewItem} />
-      <hr />
-      {list.map((item, index) => (
-        <div key={index} className="d-flex justify-content-between">
-          <h1>{item}</h1>
-          <input type="button" value="DEL" onClick={() => deleteTodo(index)} />
-        </div>
-      ))}
+    <div >
+      <h1>Like</h1>
+      <img src={displayImage} />
+      <input className="d-flex justify-content-between"type="button" value="Like Me" onClick={likeMe} />
+      <br />
+      <input type="button" value="Chnage Image" onClick={changeImage} />
+      <input type="button" value="Reset Image" onClick={resetImage} />
     </div>
   );
 }
