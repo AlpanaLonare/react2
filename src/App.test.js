e2f0815634bed4904370d19c44763607961d0cb3
Alpana Lonare <alp.lonare@gmail.com>
2023-05-23 14:43:21 +0530

    yes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { useState } from "react";
+import { useState, useRef } from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div>
+      <EventDemo />
+    </div>
+  );
+}
+// MAP OPERATOR
+// <h1>Delhi</h1>
+function EventDemo() {
+  let inputRef = useRef();
+  let [list, setList] = useState(["todo1"]);
+
+  // Member Funcn
+  let addNewItem = () => {
+    // We should not use document object
+    let inputTag = document.querySelector("#inputid");
+    let inputValue = inputTag.value;
+    console.log(inputRef.current);
+
+    let newList = [inputValue, ...list];
+    setList(newList);
+    let newValue = inputRef.current.value;
+    setList([newValue, ...list]);
+
+    inputRef.current.value = "";
+  };
+
+  // UI
+  return (
+    <div>
+      <h1>List Demo </h1>
+
+      <input type="text" placeholder="Enter Todo" id="inputid" />
+      <input type="text" placeholder="Enter Todo" ref={inputRef} />
+      <input type="button" value="Add new Item" onClick={addNewItem} />
+
+      <hr />
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </div>
+  );
+}
