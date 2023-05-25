a5d823763c887523203627d2e0e8e51af120fc34
Alpana Lonare <alp.lonare@gmail.com>
2023-05-25 16:18:57 +0530

    BMI done

--- src/bmicomponents/BMIForm.js
+++ src/bmicomponents/BMIForm.js
@@ -1,49 +1,46 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React, { useState } from 'react';
+import  Form  from "react-bootstrap/Form";
+import  Button  from "react-bootstrap/Button";
 
 function BMIForm({getData}) {
     const [height,setHeight] = useState("");
-    const [weight,setWeight] = useState("");
-const onSubmit = (e)=>
- e.preventDefault();
-if(isNaN(weight)||isNaN(height))
-{
-    console.log("Wrong input");
-}
-else
-getData(weight,height)
-
+    const [weight,setweight] = useState("");
+    const onSubmit = (e)=>
+    e.preventDefalut();
+    if(isNaN(weight)||isNaN(height))
+    {
+        console.log("Wrong input");
+    }
+    else
+    getData(weight,height)
 
   return (
     <div>
-      BMIForm for user input
-      <Form className="formbmi" onSubmit={onSubmit}>
-        <Form.Group className="FormInput" controlId="formHeight">
-          <Form.Control 
-          type="text"
-           placeholder="Height in M" 
-           value={height}
-           onChange={(e)=>setHeight(e.target.value)}
-           className="form-control"
-           />
+        BMIForms for user input
+    <Form className="Formbmi" onSubmit={onSubmit}>
+      <Form.Group className="FormInput" controlId="fromHeight">
+        <Form.Control
+        type="text"
+        placeholder="Height in M"
+        value={height}
+        onChange={(e)=>setHeight(e.target.value)}
+        className="form-control"
+        />
         </Form.Group>
         <Form.Group className="FormInput" controlId="formWeight">
-          <Form.Control 
-          type="text" 
-          placeholder="Weight in Kg" 
-          value={weight}
-          onChange={(e)=>setWeight(e.target.value)}
-          className="form-control"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Get BMI
-        </Button>
+        <Form.Control 
+        type="text" 
+        placeholder="Weight in Kg" 
+        value={weight}
+        onChange={(e) => setweight(e.target.value)}
+        className="from-control"
+        />
+      </Form.Group>
+      <Button variaint="primary" type="submit">
+        Get BMI
+      </Button>
       </Form>
     </div>
-       
   );
 }
-
 export default BMIForm;
--- src/bmicomponents/BMIScore.js
+++ src/bmicomponents/BMIScore.js
@@ -1,12 +1,14 @@
 
-import React from 'react'
+import React from "react";
 
-export default function BMISCore(props) {
- return (
-   <div>BMISCore : {props.Mybmi} <br/>
-   
-   </div>
- )
+export default function BMIScore({bmi, BmiType}) {
+  return (
+<div>
+    <h3>BMI:{bmi}</h3>
+    <h3>BMI TYPE:{BmiType}</h3>
+ </div>
+  );
 }
+
 
 
